162c0cdfa3c6176d6fca82e2572cfae3098c4dc2
shay hamalka <shayya@edu.hac.ac.il>
2022-06-13 20:11:22 +0300

    1 Hey I finished the game

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -45,10 +45,71 @@ print("GAME RULES: \n"
       "ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins\n"
       "SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win\n"
       "PAPER(2) vs ROCK(1)      --> PAPER(2) wins)\n")
+game_is_on = True
+print("helllooo welcome honey")
+
 
 # The game will run in a WHILE loop.
 # The loop is infinite, and only the user has the power to stop it (when they say they don't want to play anymore)
-while True:
+while game_is_on:
+    user_choice = input("choose one of the numbers: rock(1) paper(2) scissors(3)")
+    while user_choice != "1" and user_choice != "2" and user_choice != "3":
+        print("wrong answer choose again 1 or 2 or 3")
+        user_choice = input("choose one of the numbers: rock(1) paper(2) scissors(3)")
+    else:
+        computer_choice = random.randint(1,3)
+        user_choice = int(user_choice)
+
+    #case: user choose rock
+    if user_choice == 1 and computer_choice == 3:
+        print("yaayyy you won")
+    elif user_choice == 1 and computer_choice == 2:
+        print("damm you lost")
+    elif user_choice ==1 and computer_choice == 1:
+        print("its a draw play again")
+
+    #case: user choose paper
+    if user_choice == 2 and computer_choice == 1:
+        print("yaayyy you won")
+    elif user_choice == 2 and computer_choice == 3:
+        print("damm you lost")
+    elif user_choice == 2 and computer_choice == 2:
+        print("its a draw play again")
+
+    #case: user choose scissors
+    if user_choice == 3 and computer_choice == 2:
+        print("yaayyy you won")
+    elif user_choice == 3 and computer_choice == 1:
+        print("damm you lost")
+    elif user_choice == 3 and computer_choice == 3:
+        print("its a draw play again")
+
+    continue_request = True
+    continue_answer = input("wanna continue? y/n").lower()
+    while continue_answer != "y" and continue_answer != "n" and continue_request:
+        print("eror try again from the option u have been given")
+        continue_answer = input("wanna continue? y/n")
+    else:
+        if continue_answer == "y":
+            continue_request = False
+        elif continue_answer == "n":
+            print("thank u for playing with me see u soon")
+            game_is_on = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     """
       This is the game's heart. You'll need to think and use everything we've learned so far to make this game work.
       Remember Python's rules ( the ':' after a statement, the indentation with loops and statements..)
